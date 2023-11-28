0a92119b19b690a839b985e2c666f4f7ac3f9de3
Rodrigo Carvalho <rodrigofcc.dev@gmail.com>
2023-11-28 11:24:57 -0300

    add: scroll no cart

--- menu-espress/src/pages/shoppingCart/ShoppingCart.tsx
+++ menu-espress/src/pages/shoppingCart/ShoppingCart.tsx
@@ -1,20 +1,24 @@
 import React from "react";
-import { Text } from "react-native"
+import { Text, ScrollView } from "react-native"
 import { Card } from 'react-native-elements'
 
 const ShoppingCart = ({route}: any) => {
     const{shoppingCart} = route.params
     return (
-        shoppingCart.map((prod: any, i: number) => (
-            <>
+        <ScrollView >
+            {
+            shoppingCart.map((prod: any, i: number) => (
+
                 <Card key={i}>
                     <Card.Title style={{fontSize: 22}}> {prod.name} </Card.Title>
                     <Card.Divider/>
                     <Card.Image source={{uri: prod.image}}/>
                     <Text style={{fontSize: 16, marginEnd: "5%", marginBottom: "5%", marginTop: "3%"}}> Preço: {prod.price} </Text>
                 </Card>
-            </>
+
         ))
+        }
+        </ScrollView>
     )
 }
 
