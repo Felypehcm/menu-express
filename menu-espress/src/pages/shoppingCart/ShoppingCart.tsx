6180f30b71eaefca039811b1ed1bd71ac5536913
Rodrigo Carvalho <rodrigofcc.dev@gmail.com>
2023-11-28 10:36:58 -0300

    add: shopping card

--- menu-espress/src/pages/shoppingCart/ShoppingCart.tsx
+++ menu-espress/src/pages/shoppingCart/ShoppingCart.tsx
@@ -1,11 +1,19 @@
 import React from "react";
 import { Text } from "react-native"
+import { Card } from 'react-native-elements'
 
 const ShoppingCart = ({route}: any) => {
     const{shoppingCart} = route.params
     return (
         shoppingCart.map((prod: any, i: number) => (
-            <Text>{prod.name}</Text>
+            <>
+                <Card key={i}>
+                    <Card.Title style={{fontSize: 22}}> {prod.name} </Card.Title>
+                    <Card.Divider/>
+                    <Card.Image source={{uri: prod.image}}/>
+                    <Text style={{fontSize: 16, marginEnd: "5%", marginBottom: "5%", marginTop: "3%"}}> Preço: {prod.price} </Text>
+                </Card>
+            </>
         ))
     )
 }
