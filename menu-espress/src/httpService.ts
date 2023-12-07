43b3021f8465c8c27247f5c18f09b3b1e6d9362c
TiagoFacisa <tiago.maior@maisunifacisa.com.br>
2023-12-07 16:49:11 -0300

    Profile: Finish

--- menu-espress/src/httpService.ts
+++ menu-espress/src/httpService.ts
@@ -51,6 +51,54 @@ const httpService = {
       console.error('Erro ao atualizar a senha:', error);
       throw error;
     };
+  },
+
+  saveImageToDatabase: async (email: string, uri: string) => {
+    try {
+        const formData = new FormData();
+
+        const file = {
+            uri,
+            type: 'image/jpeg', 
+            name: 'avatar.jpg',
+        };
+        formData.append('avatar', file);
+        formData.append('email', email);
+
+        const saveResponse = await fetch(`${BASE_URL}/user/uploadAvatar`, {
+            method: 'POST',
+            body: formData,
+            headers: {
+                'Content-Type': 'multipart/form-data',
+            },
+        });
+
+        const data = await saveResponse.json();
+        console.log('Imagem salva no banco de dados:', data);
+    } catch (error) {
+        console.error('Erro ao salvar imagem no banco de dados:', error);
+    }
+  },
+
+  searchByEmail: async (email: string): Promise<{ avatar?: string }> => {
+    try {
+        const response = await fetch(`${BASE_URL}/user/findByEmail/${encodeURIComponent(email)}`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+
+        if (response.ok) {
+            const data = await response.json();
+            return data;
+        }
+
+        return {};
+    } catch (error) {
+        console.error('Erro na busca por email:', error);
+        return {};
+    }
   }
 };
 
--- menu-espress/src/pages/Profile/Profile.tsx
+++ menu-espress/src/pages/Profile/Profile.tsx
@@ -1,11 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import { View, Text, Image, Pressable, Alert } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import * as ImagePicker from 'expo-image-picker';
-import axios from 'axios'; // Certifique-se de ter o axios instalado
-import Toast from 'react-native-toast-message';
-
 import styles from './ProfileStyle';
+import * as ImagePicker from 'expo-image-picker';
+import httpService from '../../httpService';
+
 
 const Profile = ({ navigation }: any) => {
   const [userName, setUserName] = useState<string | null>(null);
@@ -17,11 +16,16 @@ const Profile = ({ navigation }: any) => {
       try {
         const name = await AsyncStorage.getItem('userName');
         const email = await AsyncStorage.getItem('userEmail');
-        const profileImage = await AsyncStorage.getItem('userProfileImage');
-
+        
         setUserName(name);
         setUserEmail(email);
-        setUserProfileImage(profileImage);
+
+        if (email) {
+          const userData = await httpService.searchByEmail(email);
+          if (userData.pathAvatar) {
+            setUserProfileImage(`data:image/jpeg;base64,${userData.pathAvatar}`);
+          }
+        }
       } catch (error) {
         console.error('Erro ao recuperar informações do usuário:', error);
       }
@@ -42,95 +46,46 @@ const Profile = ({ navigation }: any) => {
   };
 
   const goTopage = (path: string) => {
-    navigation.navigate(path)
-  }
+    navigation.navigate(path);
+  };
 
   const changePassword = () => {
-    goTopage('forgotPassword')
+    goTopage('forgotPassword');
   };
 
-  const handlePickerImage = async () => {
-    const { granted } = await ImagePicker.requestMediaLibraryPermissionsAsync();
-    if (!granted) {
-      Alert.alert(
-        'Permissão necessária',
-        'Permita que sua aplicação acesse as imagens'
-      );
-    } else {
-      const { assets, canceled } = await ImagePicker.launchImageLibraryAsync({
-        allowsEditing: true,
-        mediaTypes: ImagePicker.MediaTypeOptions.Images,
-        base64: false,
-        aspect: [4, 4],
-        quality: 1,
-      });
+  const openImagePicker = async () => {
+    const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
   
-      if (canceled) {
-        Toast.show({
-          type: 'success',
-          text1: 'Operação cancelada'
-        });
-      } else {
-        const selectedImage = assets && assets.length > 0 ? assets[0] : null;
+    if (status !== 'granted') {
+      console.log('Permissão para acessar a galeria negada');
+      return;
+    }
   
-        if (selectedImage) {
-          // Salvar a imagem no banco de dados
-          saveImageToDatabase(selectedImage.uri);
-        }
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+  
+    if (!result.canceled) {
+      const email = await AsyncStorage.getItem('userEmail');
+      
+      if (result.assets && result.assets.length > 0) {
+        const uri = result.assets[0].uri;
+        httpService.saveImageToDatabase(email, uri);
       }
     }
-  };
-
-  const saveImageToDatabase = async (imageUri: string) => {
-    try {
-      const formData = new FormData();
-      formData.append('avatar', imageUri);
-      formData.append('email', userEmail || '');
+  };  
   
-      // Chame sua API ou lógica de banco de dados aqui para salvar a URI da imagem
-      const response = await axios.post('http://10.5.7.212:8080/api/user/uploadAvatar', formData, {
-        headers: {
-          'Content-Type': 'multipart/form-data',
-        },
-      });
-  
-      // Verifique a resposta do servidor e tome as medidas apropriadas
-      if (response.status === 200) {
-        // Atualize o estado ou faça o que for necessário
-        setUserProfileImage(imageUri);
-  
-        // Salve a URI da imagem no AsyncStorage ou no estado, conforme necessário
-        AsyncStorage.setItem('userProfileImage', imageUri);  
-        Toast.show({
-          type: 'success',
-          text1: 'Imagem salva com sucesso'
-        });
-      } else {
-        Toast.show({
-          type: 'error',
-          text1: 'Erro ao salvar imagem'
-        });
-      }
-    } catch (error) {
-      console.error('Erro ao salvar imagem no banco de dados:', error);
-      Toast.show({
-        type: 'error',
-        text1: 'Erro ao salvar imagem'
-      });
-    }
-  };
-
   return (
     <View style={styles.container}>
       <View style={styles.container2}>
-        <Pressable onPress={handlePickerImage}>
-          {userProfileImage ? (
-            <Image style={styles.profileImage} source={{ uri: userProfileImage }} />
-          ) : (
-            <View style={styles.profileImagePlaceholder}>
-              <Text style={styles.profileImageText}>Adicionar Foto</Text>
-            </View>
-          )}
+        <View style={styles.profileImagePlaceholder}>
+          <Image style={styles.profileImage} source={{ uri: userProfileImage }} />
+        </View>
+        <Pressable onPress={openImagePicker} style={styles.button}>
+            <Text style={styles.buttonText}>Alterar Foto</Text>
         </Pressable>
         <View style={styles.textView}>
           <Text style={styles.text}>Nome: {userName}</Text>
