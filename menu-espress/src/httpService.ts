e4a936ec6d39c3839bf558ed6d389c6d411ac4cc
TiagoFacisa <tiago.maior@maisunifacisa.com.br>
2023-12-06 21:44:24 -0300

    Git pull error

--- menu-espress/src/httpService.ts
+++ menu-espress/src/httpService.ts
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://10.5.7.172:8080/api';
+const BASE_URL = 'http://10.5.7.212:8080/api';
 const httpService = {
   login: async (email: string, password: string) => {
     try {
